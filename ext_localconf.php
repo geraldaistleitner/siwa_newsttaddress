5415681fd056c996d852d01f12acb5186f5bf4ef
siwa_gaistleitner <gerald.aistleitner@gmail.com>
2016-07-13 10:04:03 +0200

    issue in ext_localconf.php -> wrong extension name

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,4 +3,4 @@ if (!defined('TYPO3_MODE')) {
 	die ('Access denied.');
 }
 
-$GLOBALS['TYPO3_CONF_VARS']['EXT']['news']['classes']['Domain/Model/News'][] = 'newsttaddress';
+$GLOBALS['TYPO3_CONF_VARS']['EXT']['news']['classes']['Domain/Model/News'][] = 'siwa_newsttaddress';
